5b7fc064ae2e130f5b8d55897135652a9dab6c1e
Tim Lee <tlee87@gmail.com>
2020-01-21 18:20:11 -0800

    ssl

--- src/index.js
+++ src/index.js
@@ -13,14 +13,14 @@ import Pages from "./pages";
 import "./css/tailwind.css";
 
 const wsLink = new WebSocketLink({
-  uri: `ws://ec2-18-207-208-175.compute-1.amazonaws.com:8080/query`,
+  uri: `wss://ec2-18-207-208-175.compute-1.amazonaws.com:8080/query`,
   options: {
     reconnect: true
   }
 });
 
 const httpLink = new HttpLink({
-  uri: "http://ec2-18-207-208-175.compute-1.amazonaws.com:8080/query"
+  uri: "https://ec2-18-207-208-175.compute-1.amazonaws.com:8080/query"
 });
 
 // const wsLink = new WebSocketLink({
